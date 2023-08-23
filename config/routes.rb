8288f912252d7c5b1d268b56e8a81df742cd24c6
Vincent <vincentamoguis@gmail.com>
2023-08-23 15:50:56 +0800

    chore: Added home route for landing page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   #get "/articles/:id", to: "articles#show"
   resources :articles
   resources :products
+  get "/", to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
