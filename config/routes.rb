1fd63bf12c633286405ba7f11b62e5b39daf084e
Vincent <vincentamoguis@gmail.com>
2023-09-01 16:45:31 +0800

    BREAKING CHANGES: reverted back to previous commit on added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  root "articles#index"
   #get "/articles", to: "articles#index"
   #get "/articles/:id", to: "articles#show"
   resources :articles
   resources :products
+  get "/", to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
