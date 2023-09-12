78d551f5b8171eca890f50901f3d5f86c6adbbb4
Vincent <vincentamoguis@gmail.com>
2023-09-12 16:14:30 +0800

    refactor(db/seed):Modified the seed file to the new schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,15 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-processors = [
-  { brand: "AMD", description: "Ryzen 2"},
-  { brand: "Intel", description: "Pentium Gold"},
-  { brand: "AMD", description: "Threadripper"}
+products = [
+  { type: "Processor", brand: "AMD", name: "Ryzen 3", description: "LGA1200", price: 1200 },
+  { type: "Processor", brand: "Intel", name: "Intel i3", description: "65W 6mb 4C/8T", price: 2300 },
+  { type: "Processor", brand: "Intel", name: "Intel i9", description: "65W 12mb 8C/1T", price: 5000 },
+  { type: "Processor", brand: "AMD", name: "Ryzen 7", description: "5700G 8C/16T 19mb", price: 9000 },
+  { type: "Processor", brand: "AMD", name: "Ryzen 7", description: "5800X 8C/16T 36mb", price: 13387 },
+  { type: "Processor", brand: "AMD", name: "Ryzen 5", description: "7600 6C/12T 32mb", price: 12995 }
 ]
 
-processors.each do |processor|
-  Processor.create(processor)
+products.each do |product|
+  Product.create(product)
 end
